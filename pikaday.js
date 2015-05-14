40fb0b5c39401e61963d42f48d48ec37519971bf
Yoni Jah <yonjah@gmail.com>
2015-05-14 13:54:44 +0800

    when using setMaxDate it should update maxYear and maxMonth

--- pikaday.js
+++ pikaday.js
@@ -637,9 +637,7 @@
                 this.setMinDate(opts.minDate);
             }
             if (opts.maxDate) {
-                setToStartOfDay(opts.maxDate);
-                opts.maxYear  = opts.maxDate.getFullYear();
-                opts.maxMonth = opts.maxDate.getMonth();
+                this.setMaxDate(opts.maxDate);
             }
 
             if (isArray(opts.yearRange)) {
@@ -834,7 +832,10 @@
          */
         setMaxDate: function(value)
         {
-            this._o.maxDate = value;
+            setToStartOfDay(value);
+            this._o.maxDate  = value;
+            this._o.maxYear  = value.getFullYear();
+            this._o.maxMonth = value.getMonth();
         },
 
         setStartRange: function(value)
